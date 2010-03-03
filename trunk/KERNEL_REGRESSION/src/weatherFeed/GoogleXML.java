c4b2a7c170832b455d46f3acb3adbc79e63954ea
matthieu.nahoum <matthieu.nahoum@3e0cce10-258a-11df-8ea0-49ba94de29ab>
2010-03-03 22:45:23 +0000

    Google feed

--- trunk/KERNEL_REGRESSION/src/weatherFeed/GoogleXML.java
+++ trunk/KERNEL_REGRESSION/src/weatherFeed/GoogleXML.java
@@ -10,7 +10,7 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 /**
- *
+ * Class retrieving the data coming from the Google Weather feed
  * @author Matthieu Nahoum
  */
 public class GoogleXML {
@@ -22,14 +22,22 @@ public class GoogleXML {
     }
 
     private GoogleXML() {
+        /**
+         * Later, put this in an upper main class
+         */
         this.cities = new HashMap<String, String>();
         this.cities.put("Berkeley, CA", "http://www.google.com/ig/api?weather=Berkeley,CA&hl=en");
         this.cities.put("San Francisco, CA", "http://www.google.com/ig/api?weather=San+Francisco,CA&hl=en");
         this.cities.put("Los Angeles, CA", "http://www.google.com/ig/api?weather=Los+Angeles,CA&hl=en");
+        this.cities.put("San Diego, CA", "http://www.google.com/ig/api?weather=San+Diego,CA&hl=en");
         this.cities.put("Austin, TX", "http://www.google.com/ig/api?weather=Austin,TX&hl=en");
         this.cities.put("Chicago, IL", "http://www.google.com/ig/api?weather=Chicago,IL&hl=en");
         this.cities.put("Paris, IDF", "http://www.google.com/ig/api?weather=Paris,IDF&hl=en");
         this.cities.put("Brisbane, Australia", "http://www.google.com/ig/api?weather=Brisbane,Australia&hl=en");
+        this.cities.put("Beijing, China", "http://www.google.com/ig/api?weather=Beijing,China&hl=en");
+        this.cities.put("Omaha, NE", "http://www.google.com/ig/api?weather=Omaha,NE&hl=en");
+        this.cities.put("Washington, DC", "http://www.google.com/ig/api?weather=Washington,DC&hl=en");
+        this.cities.put("New York City, NY", "http://www.google.com/ig/api?weather=New+York+City,NY&hl=en");
         try {
             for (String city : this.cities.keySet()) {
                 this.parse(city, this.cities.get(city));
@@ -39,6 +47,17 @@ public class GoogleXML {
         }
 
     }
+
+    /**
+     * Types of data feeds observed concerning weather
+     * Sunny
+     * Partly Cloudy
+     * Cloudy
+     * Mostly Cloudy
+     * Overcast
+     * Showers
+     * Rain
+     */
 
     /**
      * Parse the feed and get the weather data
@@ -59,6 +78,7 @@ public class GoogleXML {
         String windSpeed = "";
         String windDirection = "";
 
+        // Loop through and edit the XML tree.
         ArrayList<Node> children = new ArrayList<Node>();
         for (int k = 0; k < root.getChildNodes().getLength(); k++) {
             children.add(root.getChildNodes().item(k));
@@ -81,6 +101,7 @@ public class GoogleXML {
         for (int k = 0; k < root.getChildNodes().getLength(); k++) {
             children.add(root.getChildNodes().item(k));
         }
+        //Get the current weather conditions
         for (Node child : children) {
             if (child.getNodeName().equals("condition")) {
                 NamedNodeMap attributes = child.getAttributes();
@@ -117,11 +138,19 @@ public class GoogleXML {
                 }
             }
         }
+        // Print out
+        // TODO remove
         System.out.println("City:" + city
                 + "\nCondition: " + condition
                 + ", Temperature: " + temperature
                 + " F, Humidity: " + humidity
                 + ", Wind direction: " + windDirection
                 + ", Wind speed: " + windSpeed + ".\n");
+
+        /**
+         * Handle the database connections to store the data
+         * Maybe create a static DB object
+         */
+
     }
 }
