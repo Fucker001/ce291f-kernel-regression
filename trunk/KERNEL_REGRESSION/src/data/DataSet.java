9d6dec5317e5e39df49274aa5feafe60c9624288
matthieu.nahoum <matthieu.nahoum@3e0cce10-258a-11df-8ea0-49ba94de29ab>
2010-03-07 05:03:57 +0000

    dataset

--- trunk/KERNEL_REGRESSION/src/data/DataSet.java
+++ trunk/KERNEL_REGRESSION/src/data/DataSet.java
@@ -1,12 +1,46 @@
 package data;
 
+import Jama.Matrix;
+
 /**
  *
  * @author Matthieu Nahoum
  */
 public class DataSet {
 
-    private Input input;
-    private Output output;
+    private Matrix input;
+    private Matrix output;
+
+    private DataSet(){
+
+    }
+
+    public static DataSet concatenate(DataSet left, DataSet right){
+        DataSet result = null;
+
+        return result;
+    }
+
+    public Matrix getModifiedInput(){
+        Matrix result = null;
+
+        return result;
+    }
+
+    /**
+     * @return the input
+     */
+    public Matrix getInput() {
+        return input;
+    }
+
+    /**
+     * @return the output
+     */
+    public Matrix getOutput() {
+        return output;
+    }
+
+
 
 }
