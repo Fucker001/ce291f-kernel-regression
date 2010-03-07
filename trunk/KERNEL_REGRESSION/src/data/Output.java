cd412bb67f3d2405aab1952699845923d54b9cef
matthieu.nahoum <matthieu.nahoum@3e0cce10-258a-11df-8ea0-49ba94de29ab>
2010-03-07 04:17:22 +0000

    outputs

---
+++ trunk/KERNEL_REGRESSION/src/data/Output.java
@@ -0,0 +1,24 @@
+
+package data;
+
+import Jama.*;
+
+/**
+ *
+ * @author Matthieu Nahoum
+ */
+public class Output {
+
+    private Matrix matrix;
+
+    public Output() {
+        this.matrix = new Matrix(SampleData.outputs);
+    }
+
+    private void load(){
+
+    }
+
+
+
+}
