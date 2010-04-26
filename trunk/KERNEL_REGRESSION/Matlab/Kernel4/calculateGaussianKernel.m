ee05a3d82f18281eb9e48f3eeeae49ea5e5c9ab0
matthieu.nahoum <matthieu.nahoum@3e0cce10-258a-11df-8ea0-49ba94de29ab>
2010-04-26 20:50:15 +0000

    Working 3d version of kernel

---
+++ trunk/KERNEL_REGRESSION/Matlab/Kernel4/calculateGaussianKernel.m
@@ -0,0 +1,27 @@
+% Calculates and manipulates a gaussian Kernel of parameter sigma.
+% Does an SVD and returns the US^0.5 matrix.
+
+function [GaussianKernel] = calculateGaussianKernel(Inputs,Sigma)
+
+global NumberOfPoints
+
+%% Calculates the Gaussian Kernel
+Kernel = zeros(NumberOfPoints,NumberOfPoints);
+
+for i = 1:NumberOfPoints
+    for j = 1:NumberOfPoints
+        Kernel(i,j) = (norm(Inputs(:,i)-Inputs(:,j),2))^2;
+    end
+end
+Kernel = exp(-Kernel/(2*Sigma^2));
+
+% Normalisation
+N = trace(Kernel);
+Kernel = Kernel/N;
+
+%% Does an SVD and returns the US^0.5 matrix.
+
+[U,S] = svd(Kernel);
+GaussianKernel = U*(S^0.5);
+disp('Gaussian Kernel SVD finished')
+toc
